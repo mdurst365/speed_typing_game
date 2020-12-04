33b0635bf1c7381bbefd9bd432e84f7020d78fd4
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-04 14:25:05 -0500

    updated js to split quotes into single characters

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const random_quote_API_URL = 'https://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
 
 function getRandomQuote() {
 return fetch(random_quote_API_URL)
-    .then(response =/ response.json())
+    .then(response => response.json())
     .then(data => data.content)
 }
 
-async function getNextQuote(){
+async function renderNewQuote() {
     const quote = await getRandomQuote()
-    console.log(quote)
+    quoteDisplayElement.innerHTML = ''
+    quote.split('').forEach(character => {
+      const characterSpan = document.createElement('span')
+      characterSpan.innerText = character
+      quoteDisplayElement.appendChild(characterSpan)
+    })
+    quoteInputElement.value = null
 }
 
-getNextQuote()
+renderNewQuote()
+
